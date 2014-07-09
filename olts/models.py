eb3aae5d4dc9a2451fc47df33273b33b6b5fae42
mfantonidas <7225888>
2014-07-09 13:08:08 +0800

    201407091308

--- olts/models.py
+++ olts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Olts(models.Module):
+    name = models.CharField(max_length=50,unique=True)
+	ip = models.CharField(max_length=16,unique=True)
+	upswitch = models.CharField(max_length=50,unique=True,blank=True,null=True)
+	
