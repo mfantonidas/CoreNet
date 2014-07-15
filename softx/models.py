ed1cfe500d1d33806852aeff8ba4845eda8122a2
mfantonidas <15305732612@189.cn>
2014-07-15 23:37:14 +0800

    111

--- softx/models.py
+++ softx/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class corenet_ne(models.Module):
+    name = models.CharField(max_length=50,unique=True)
+	ipaddr = models.IPAddressField(unique=True)
+	netype = models.CharField(max_length=20,unique=True)
+	snmpaddr = models.IPAddressField(blank = True)
+	uplink = models.CharField(max_length=50,blank=True)
+	
+class softxinfo(models.Module):
+    
