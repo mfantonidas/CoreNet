d07e54ccb8a22186971d8e98b2fa71fca97d30b0
root <root@localhost.localdomain>
2014-07-17 23:07:57 +0800

    201407172306

--- tips/admin.py
+++ tips/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from tips.models import contact_info
 
-admin.site.register(contact_info)
+class contactAdmin(admin.ModelAdmin):
+    list_display = ('department', 'tel')
+    search_fields = ('department', 'tel')
+
+admin.site.register(contact_info, contactAdmin)
