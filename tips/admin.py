2b6e386b53bb3cbe87d7b37df2b4126adf892cb9
root <root@localhost.localdomain>
2014-07-18 10:39:41 +0800

    222

--- tips/admin.py
+++ tips/admin.py
@@ -4,5 +4,6 @@ from tips.models import contact_info
 class contactAdmin(admin.ModelAdmin):
     list_display = ('department', 'tel')
     search_fields = ('department', 'tel')
+    ordering = ('id',)
 
 admin.site.register(contact_info, contactAdmin)
