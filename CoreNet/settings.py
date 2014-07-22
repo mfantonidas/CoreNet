2ef6539928b9bdae95aa65be859e262bebf6304c
mfantonidas <7225888>
2014-07-22 22:25:15 +0800

    111

--- CoreNet/settings.py
+++ CoreNet/settings.py
@@ -58,17 +58,20 @@ MEDIA_ROOT = ''
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
+import os
+SITE_ROOT=os.path.join(os.path.abspath(os.path.dirname(__file__)),'..')
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(SITE_ROOT,'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '../static/'
 
-import os.path
+
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
