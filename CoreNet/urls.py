c8bfd24de7a486148bea38bcf9a5828fda412324
mfantonidas <15305732612@189.cn>
2014-07-28 16:49:45 +0800

    121

--- CoreNet/urls.py
+++ CoreNet/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import *
-from CoreNet.views import hello, corenet
+#from CoreNet.views import hello, corenet
 from CoreNet.views import login_view, logout_view
 import testapp
 from testapp import urls
@@ -18,8 +18,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^hello/$', hello),
-    url(r'^hello1/', include(testapp.urls)),
+    #url(r'^hello/$', hello),
+    #url(r'^hello1/', include(testapp.urls)),
     url(r'^corenet/$', "CoreNet.views.corenet",name='main_page'),
 	url(r'^accounts/login$', "CoreNet.views.login_view", name='accounts_login'),
 	url(r'^accounts/logout$', "CoreNet.views.logout_view", name='accounts_logout'),
