c8d964db03da1e7420fde08f7de3ed852d58c87d
root <root@localhost.localdomain>
2014-08-05 21:23:22 +0800

    asdf

--- CoreNet/views.py
+++ CoreNet/views.py
@@ -27,7 +27,7 @@ def corenet(request):
     if request.user.is_authenticated():
         user = request.user
 	contact = contact_info()
-	return render_to_response('corenet_admin/corenet.html', {'user':user, 'contact':contact_info},context_instance=RequestContext(request))
+	return render_to_response('corenet_admin/corenet.html', {'user':user, 'contact':contact},context_instance=RequestContext(request))
     else:
 	return login(request)
 
