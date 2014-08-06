85c089e0051d2caf906c5322bed9540c6e77a7be
mfantonidas <15305732612@189.cn>
2014-08-06 11:13:36 +0800

    123123

--- tips/admin.py
+++ tips/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from tips.models import contact_info
 
 class contactAdmin(admin.ModelAdmin):
-    list_display = ('department', 'tel')
-    search_fields = ('department', 'tel')
+    list_display = ('department', 'tel', 'duty')
+    search_fields = ('department', 'tel', 'duty')
     ordering = ('id',)
 
 admin.site.register(contact_info, contactAdmin)
