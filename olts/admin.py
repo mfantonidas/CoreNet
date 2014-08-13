80db16bf091af66e0905aa248155456c1580d1a5
mfantonidas <7225888>
2014-08-13 21:56:01 +0800

    asdf

--- olts/admin.py
+++ olts/admin.py
@@ -2,32 +2,32 @@ from django.contrib import admin
 from CoreNE.models import *
 
 class OltsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'ipaddr', 'netype')
-    search_fields = ('name', 'ipaddr', 'netype')
-    ordering = ('netype',)
+    list_display = ('name', 'ip', 'upswitch', 'type', 'odf', 'area', 'upbandwidth')
+    search_fields = ('name', 'ipaddr')
+    ordering = ('ip',)
 
 class fttoAdmin(admin.ModelAdmin):
-    list_display = ('locate', 'area')
-    search_fields = ('locate', 'area')
-    ordering = ('locate',)
+    list_display = ('name', 'oltip', 'sn', 'obd', 'pvlan', 'cvlan', 'ording', 'description')
+    search_fields = ('name', 'sn')
+    ordering = ('name',)
 
 class fttxAdmin(admin.ModelAdmin):
-    list_display = ('area',)
-    search_fields = ('area',)
-    ordering = ('area',)
+    list_display = ('name', 'manage_ip', 'voice_ip', 'oltip')
+    search_fields = ('name', 'manage_ip', 'voice_ip', 'oltip',)
+    ordering = ('name',)
 
 class dataprojectAdmin(admin.ModelAdmin):
-    list_display = ('area',)
+    list_display = ('area', 'olt', 'vlantype', 'voicetype')
     search_fields = ('area',)
     ordering = ('area',)
 
-class numbersrcAdmin(admin.ModelAdmin):
-    list_display = ('area',)
-    search_fields = ('area',)
-    ordering = ('area',)
+#class numbersrcAdmin(admin.ModelAdmin):
+#    list_display = ('',)
+#    search_fields = ('area',)
+#    ordering = ('area',)
 	
 admin.site.register(Olts, OltsAdmin)
-admin.site.register(Ftto, fttoAdmin)
-admin.site.register(Fttx, fttxAdmin)
-admin.site.register(Dataproject, dataprojectAdmin)
-admin.site.register(Numbersrc, numbersrcAdmin)
+admin.site.register(ftto, fttoAdmin)
+admin.site.register(fttx, fttxAdmin)
+admin.site.register(dataproject, dataprojectAdmin)
+#admin.site.register(Numbersrc, numbersrcAdmin)
