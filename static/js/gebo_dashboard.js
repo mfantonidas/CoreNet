3f740460d437746fe7a6cccc875ebb9a153f08ad
mfantonidas <15305732612@189.cn>
2014-08-14 16:46:04 +0800

    asdfds

--- static/js/gebo_dashboard.js
+++ static/js/gebo_dashboard.js
@@ -262,7 +262,7 @@
 				//var search_email = $(this).find('.sl_email').text();
 				list_source.push(search_name);
 			});
-			$('.olt-list-search').typeahead({source: list_source, items:20});
+			$('.olt-list-search').typeahead({source: list_source, items:50});
 			
 			var pagingOptions = {};
 			var options = {
@@ -278,37 +278,6 @@
 				]
 			};
 			var userList = new List('olt-list', options);
-			
-			$('#filter-online').on('click',function() {
-				$('ul.filter li').removeClass('active');
-				$(this).parent('li').addClass('active');
-				userList.filter(function(item) {
-					if (item.values().sl_status == "online") {
-						return true;
-					} else {
-						return false;
-					}
-				});
-				return false;
-			});
-			$('#filter-offline').on('click',function() {
-				$('ul.filter li').removeClass('active');
-				$(this).parent('li').addClass('active');
-				userList.filter(function(item) {
-					if (item.values().sl_status == "offline") {
-						return true;
-					} else {
-						return false;
-					}
-				});
-				return false;
-			});
-			$('#filter-none').on('click',function() {
-				$('ul.filter li').removeClass('active');
-				$(this).parent('li').addClass('active');
-				userList.filter();
-				return false;
-			});
 			
 			$('#olt-list').on('click','.sort',function(){
 					$('.sort').parent('li').removeClass('active');
