bca698f63ddd6b01392058bdc903ca38b34157bb
mfantonidas <15305732612@189.cn>
2014-08-19 15:19:00 +0800

    asdf

--- CoreNE/adminx.py
+++ CoreNE/adminx.py
@@ -1,21 +1,25 @@
-from django.contrib import admin
+#from django.contrib import admin
+import xadmin
 from CoreNE.models import corenet_ne, softxinfo, IMSinfo
 
-class coreneAdmin(admin.ModelAdmin):
+#class coreneAdmin(admin.ModelAdmin):
+class coreneAdmin(object):
     list_display = ('name', 'ipaddr', 'netype')
     search_fields = ('name', 'ipaddr', 'netype')
     ordering = ('netype',)
 
-class softxAdmin(admin.ModelAdmin):
+#class softxAdmin(admin.ModelAdmin):
+class softxAdmin(object):
     list_display = ('locate', 'area')
     search_fields = ('locate', 'area')
     ordering = ('locate',)
 
-class IMSAdmin(admin.ModelAdmin):
+#class IMSAdmin(admin.ModelAdmin):
+class IMSAdmin(object):
     list_display = ('area',)
     search_fields = ('area',)
     ordering = ('area',)
 
-admin.site.register(corenet_ne, coreneAdmin)
-admin.site.register(softxinfo, softxAdmin)
-admin.site.register(IMSinfo, IMSAdmin)
+xadmin.site.register(corenet_ne, coreneAdmin)
+xadmin.site.register(softxinfo, softxAdmin)
+xadmin.site.register(IMSinfo, IMSAdmin)
