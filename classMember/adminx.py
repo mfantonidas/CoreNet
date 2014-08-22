791c3570b2e97b5815f5e26d6fbd3e967485f225
mfantonidas <15305732612@189.cn>
2014-08-22 16:19:40 +0800

    asdfa

--- classMember/adminx.py
+++ classMember/adminx.py
@@ -5,8 +5,14 @@ from xadmin.views import BaseAdminPlugin, ListAdminView
 import xadmin
 from tips.models import contact_info
 
+class TestAdminPlugin(BaseAdminPlugin):
+    def get_context(self,context):
+	    context['test'] = True
+		return context
+		
 #class upload_duty(BaseAdminPlugin):
 #    say_hello = False
 
 # Register your models here.
 #xadmin.site.register(, )
+site.register_plugin(TestAdminPlugin, ListAdminView)
