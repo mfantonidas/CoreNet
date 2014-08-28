64e1935924a9a2a7b5c71151994e44d166746179
mfantonidas <15305732612@189.cn>
2014-08-28 09:17:26 +0800

    asdfafs

--- tips/adminx.py
+++ tips/adminx.py
@@ -1,11 +1,16 @@
 #from django.contrib import admin
 import xadmin
+from xadmin import views
 from tips.models import contact_info
 
+class GlobalSetting(object):
+    site_title = 'CoreNet'
+	site_footer = 'CoreNet'
 #class contactAdmin(admin.ModelAdmin):
 class contactAdmin(object):
     list_display = ('department', 'tel', 'duty')
     search_fields = ('department', 'tel', 'duty')
     ordering = ('id',)
 
+xadmin.site.register(views.CommAdminView, GlobalSetting)
 xadmin.site.register(contact_info, contactAdmin)
